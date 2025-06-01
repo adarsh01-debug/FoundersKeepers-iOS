05ead3d191b316b7cd29d986b96a391fb8cc1680
Adarsh <adarsh.official2299@gmail.com>
2025-06-01 22:35:46 +0530

    Initial Commit

---
+++ FoundersKeepers/FoundersKeepersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundersKeepersApp.swift
+//  FoundersKeepers
+//
+//  Created by Adarsh   on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundersKeepersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
